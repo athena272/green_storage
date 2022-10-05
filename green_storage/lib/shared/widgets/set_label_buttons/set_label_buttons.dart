10eccd613d239742dec789e2c852a93746af829f
athena272 <guilhermera272@gmail.com>
2022-10-05 13:52:05 -0300

    Update set_label_buttons.dart

--- green_storage/lib/shared/widgets/set_label_buttons/set_label_buttons.dart
+++ green_storage/lib/shared/widgets/set_label_buttons/set_label_buttons.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 import 'package:payflow/shared/widgets/divider_vertical/divider_vertical_widget.dart';
 import 'package:payflow/shared/widgets/label_button/label_button.dart';
 
@@ -7,17 +9,20 @@ class SetLabelButtons extends StatelessWidget {
   final VoidCallback primaryOnPressed;
   final String secondaryLabel;
   final VoidCallback secondaryOnPressed;
+  final bool enablePrimaryColor;
   const SetLabelButtons({
     Key? key,
     required this.primaryLabel,
     required this.primaryOnPressed,
     required this.secondaryLabel,
     required this.secondaryOnPressed,
+    this.enablePrimaryColor = false,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: AppColors.shape,
       height: 56,
       child: Row(
         children: [
@@ -25,6 +30,7 @@ class SetLabelButtons extends StatelessWidget {
             child: LabelButton(
               label: primaryLabel,
               onPressed: primaryOnPressed,
+              style: enablePrimaryColor ? TextStyles.buttonBoldPrimary : null,
             ),
           ),
           DividerVerticalWidget(),
